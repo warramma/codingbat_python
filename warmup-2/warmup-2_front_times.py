0c270240a6b5fd4f1a96f25bdf968cef0692bf9c
warramma <my email address used on my git repo>
2024-09-03 10:17:56 -0500

    solve front_tiems

---
+++ warmup-2/warmup-2_front_times.py
@@ -0,0 +1,14 @@
+#Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+#front_times('Chocolate', 2) → 'ChoCho'
+#front_times('Chocolate', 3) → 'ChoChoCho'
+#front_times('Abc', 3) → 'AbcAbcAbc'
+
+def front_times(str, n):
+  front = ''
+  if len(str) < 3:
+    front = str
+  else:
+    front = str[0:3]
+  
+  return front * n
